1d659e6bf79d0bb988cf4722779223fcd6b4ee82
Jacob Legg <skeletalsoup@protonmail.com>
2018-06-13 16:15:32 -0400

    added extra function to ask for more cheese

--- Pizza_App.rb
+++ Pizza_App.rb
@@ -1,5 +1,6 @@
-puts "How many pizzas would you like to order"
-pizza_count = get.chomp
+puts "Howdy, welcome to Pep-peps Pizza. How many pizzas would you like to order?"
+pizza_count = gets.chomp.to_i
+
 
 
 def pizza_build
@@ -31,4 +32,19 @@ def special()
     special = ["pineapple", "tiny-fish", "coleslaw"]
 end
 
-pizza_build
+while pizza_count > 0 do
+    pizza_build
+    pizza_count -= 1
+end
+
+puts "Would you like extra cheese on your Pep-pep Pie? [yes] [no]"
+
+pizza_extra = gets.chomp
+1.times do pizza_extra
+"pizza_extra is #{pizza_extra.class}"
+ if pizza_extra == "yes"
+    puts "Extra Cheese has been added"
+ else pizza_extra == "no"
+    puts "Enjoy your Pep-pep Pie, goodpie"
+ end
+end
